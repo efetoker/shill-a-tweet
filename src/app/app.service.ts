cc2119b46dea056424ce0b60972f7d7a572f647f
Efe TOKER <e.toker@medyatakip.com>
2022-03-30 00:33:39 +0300

    up

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -11,10 +11,10 @@ export class AppService {
   }
 
   getAccounts() {
-    return this.http.get('https://twitter-authenticator.herokuapp.com/api/accounts');
+    return this.http.get('https://twitter-authenticator.herokuapp.com/accounts');
   }
 
   deleteAccount(account: any) {
-    return this.http.delete('https://twitter-authenticator.herokuapp.com/api/account/' + account.id);
+    return this.http.delete('https://twitter-authenticator.herokuapp.com/account/' + account.id);
   }
 }
